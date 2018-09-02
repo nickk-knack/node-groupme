13b98ce02af1e7f2ab76788a7721ac7024f55c16
nickk-knack <nick.chiconas@gmail.com>
2018-09-01 22:12:41 -0500

    removed dependency on non-existent module

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 bot.initialize({
 	bot_id: process.env.BOT_ID,
 	group_id: process.env.GROUP_ID,
-	modules: ['giphy', 'dice', 'e621']
+	modules: ['giphy', 'dice']
 });
 
 app.use(bodyParser.json());
