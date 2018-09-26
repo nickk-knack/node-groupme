4e673df06862c6224b6b8f1ae8a4a41a40bafee9
nickk-knack <nick.chiconas@gmail.com>
2018-09-26 10:47:41 -0500

    fixed it, idk how

--- modules/inspire.js
+++ modules/inspire.js
@@ -6,18 +6,12 @@ exports.process = (message, bot) => {
 
 	if (index != -1) {
 		const url = 'http://inspirobot.me/api?generate=true';
-		console.log(url);
-
 		bot.request.get(url, (err, resp, body) => {
-			console.log('resp', resp);
-			console.log('body', body);
 			if (err) {
 				bot.sendMessage('uh oh, something is fucky wucky UwU');
 			} else {
 				bot.sendMessage(body);
 			}
 		});
-	} else {
-		console.log('did not find .inspire');
 	}
 };
