43d4f9be2019c31ba5c508f86b1a72791d197aa2
nickk-knack <nick.chiconas@gmail.com>
2018-09-26 22:03:49 -0500

    added quotes for piao module

--- modules/piao.js
+++ modules/piao.js
@@ -7,6 +7,7 @@ const quotes = [
 	'Switches are so fucking sexy, UNF',
 	'Any deviation from my written solution will be cause for problems.',
 	'I bring mug with many handles. It explains circuits.',
+	'Give me lamp, brother.',
 ];
 
 exports.process = (message, bot) => {
