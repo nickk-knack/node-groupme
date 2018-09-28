5195609f8522cae6e12e5984d48afa7af406eb8d
nickk-knack <nick.chiconas@gmail.com>
2018-09-28 10:17:26 -0500

    add 8ball module

---
+++ modules/8ball.js
@@ -0,0 +1,36 @@
+const responses = [
+	'It is certain.', 
+	'It is decidedly so.',
+	'Without a doubt.',
+	'Yes, definitely.',
+	'You may rely on it.',
+	'As I see it, yes.',
+	'Most likely.',
+	'The outlook is good.',
+	'Yes.',
+	'Fuck ya dude',
+	'Reply hazy, try again.',
+	'Ask again later.',
+	'Better not tell you now...',
+	'Cannot predict now.',
+	'Maybe??? Why the fuck would I know that?',
+	'Don\'t count on it.',
+	'My reply is no.',
+	'My sources say no.',
+	'Very doubtful.',
+	'Fuck no, bro.',
+	'Absolutely the fuck not.',
+	'Grilled cheese'
+];
+
+exports.process = (message, bot) => {
+	if (message.is_bot) return;
+
+	const command = '.8ball ';
+	const index = message.text.toLowerCase().indexOf(command);
+
+	if (index != -1) {
+		const response = responses[Math.floor(Math.random() * responses.length)];
+		bot.sendMessage(`@${message.name}, ${response}`);
+	}
+};
