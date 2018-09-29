d56b02cf9110669b9b5876a8146fe7bbb5db4e3f
nickk-knack <nick.chiconas@gmail.com>
2018-09-29 17:48:00 -0500

    begin implementing new command processing

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const bot = require('./bot.js');
-
+const commandModules = new Map();
+const commandFiles = fs.readdirSync('./modules');
+const cooldowns = new Map();
+const prefix = process.env.PREFIX || '.';
 const port = process.env.PORT || 3000;
+
+// Check that we have a bot_id and group_id before we start loading shit
+if (!process.env.BOT_ID) {
+	console.error('No BOT_ID supplied!');
+	process.exit(1);
+}
+
+if (!process.env.GROUP_ID) {
+	console.error('No GROUP_ID supplied!');
+	process.exit(1);
+}
+
+// Load all command modules
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commandModules.set(command.name, command);
+}
 
 bot.initialize({
 	bot_id: process.env.BOT_ID,
--- modules/help.js
+++ modules/help.js
@@ -1,10 +1,18 @@
-exports.process = (message, bot) => {
-	if (message.is_bot) return;
+module.exports = {
+	name: '',
+	aliases: [],
+	description: '',
+	usage: '',
+	args: false,
+	cooldown: 10,
+	process(message, bot) {
+		if (message.is_bot) return;
 
-	const command = '.help';
-	const index = message.text.toLowerCase().indexOf(command);
+		const command = '.help';
+		const index = message.text.toLowerCase().indexOf(command);
 
-	if (index != -1) {
-		bot.sendMessage('Commands: .help, .thank, .bad, .d <# of sides>, .inspire, .piao, .giphy <search terms>, .images <search terms>, .uwu <search terms>, .8ball <question>');
-	}
+		if (index != -1) {
+			bot.sendMessage('Commands: .help, .thank, .bad, .d <# of sides>, .inspire, .piao, .giphy <search terms>, .images <search terms>, .uwu <search terms>, .8ball <question>');
+		}
+	},
 };
