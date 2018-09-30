89e4286d6a791307897beb154f5322ebc9fd5065
nickk-knack <nick.chiconas@gmail.com>
2018-09-30 17:25:51 -0500

    remove console logs

--- modules/pornhub.js
+++ modules/pornhub.js
@@ -10,8 +10,6 @@ module.exports = {
 	execute(message, args, bot) {
 		const contentType = args.shift().toLowerCase();
 
-		console.log(contentType);
-
 		if (contentType != 'gif' && contentType != 'vid') {
 			bot.sendMessage(`@${message.name} you need to specify if you want a gif or a vid.`);
 			return;
@@ -19,18 +17,15 @@ module.exports = {
 		
 		const query = args.join(' ');
 		const search = new Pornsearch(query);
-		console.log(query);
 		
 		if (contentType == 'gif') {
 			search.gifs()
 				.then(gifs => {
-					console.log(gifs);
 					bot.sendMessage(gifs[Math.floor(Math.random() * gifs.length)].url);
 				});
 		} else if (contentType == 'vid') {
 			search.videos()
 				.then(vids => {
-					console.log(vids);
 					bot.sendMessage(vids[Math.floor(Math.random() * vids.length)].url);
 				});
 		}
