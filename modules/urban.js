3b97d92038e173e1edcb44f78caf5edd739db20a
nickk-knack <nick.chiconas@gmail.com>
2018-09-30 14:09:45 -0500

    add urbandictionary module

---
+++ modules/urban.js
@@ -0,0 +1,28 @@
+const snekfetch = require('snekfetch');
+const trim = (str, max) => (str.length > max) ? `${str.slice(0, max - 3)}...` : str;
+
+module.exports = {
+	name: 'urban',
+	aliases: ['ud', 'urband'],
+	description: 'Searches urbandictionary.com for a phrase and returns the closest listing.',
+	usage: '<search>',
+	args: true,
+	cooldown: 5,
+	execute(message, args, bot) {
+		snekfetch.get('https://api.urbandictionary.com/v0/define').query({ term: args.join(' ') })
+			.then(data => data.body)
+			.then (body => {
+				if (body.result_type === 'no_results') {
+					bot.sendMessage(`No results found for **${args.join(' ')}**`);
+					return;
+				}
+
+				const [answer] = body.list;
+				let msg = '';
+				msg += `${answer.word}: \n`;
+				msg += `Definition: ${trim(answer.definition, 256)}\n`;
+				msg += `Example: ${answer.example}\n`;
+				bot.sendMessage(msg);
+			});		
+	},
+};
