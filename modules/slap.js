2b9c5ef191334eb58d46f169146898de0bb495bc
nickk-knack <nick.chiconas@gmail.com>
2018-09-30 00:32:10 -0500

    add slap module

---
+++ modules/slap.js
@@ -0,0 +1,46 @@
+const items = ['a giant fish',
+	'a trout',
+	'a frying pan',
+	'a whip',
+	'a fuckin\' cock',
+	'a dildo',
+	'his 5.3 inch dick',
+	'a shoe',
+	'a used napkin',
+	'a mexican midget',
+	'a rubber chicken',
+	'an STD',
+	'a condom',
+	'a pair of flip flops',
+	'a wet noodle',
+	'a single potato chip',
+	'a flatbill',
+	'a trout',
+	'my big ol\' wang',
+	'a classy cocaine purse',
+	'a scientology pamphlet',
+];
+
+module.exports = {
+	name: 'slap',
+	description: 'Virtually slaps a user',
+	usage: '<user>',
+	args: false,
+	cooldown: 1,
+	execute(message, args, bot) {
+		if (!args.length) {
+			bot.sendMessage(`*slaps ${message.name} with a fuckin' stick*`);
+			return;
+		}
+
+		const user = args[0];
+		args.shift();
+		let item = items[Math.floor(Math.random() * items.length)];
+		if (args[0]) {
+			// get rest of args, concat into string, make that the thing to slap with
+			item = args.join(' ');
+		}
+
+		bot.sendMessage(`*slaps ${user} with ${item}!*`);
+	},
+};
