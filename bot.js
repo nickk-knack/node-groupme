c3b1ef253f7764a640bbfa5ec5e041caf8921df3
nickk-knack <nick.chiconas@gmail.com>
2018-10-05 09:46:56 -0500

    revert previous change

--- bot.js
+++ bot.js
@@ -37,8 +37,6 @@ const bot = {
 
 // Handler for when a message is received
 exports.onPost = (req, res) => {
-	console.log(req.body);
-
 	// Create message object from input
 	const message = {
 		text: req.body.text,
