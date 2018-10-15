f8dac719daf92ada94e29204114b73246ccbc61e
nickk-knack <nick.chiconas@gmail.com>
2018-10-15 09:26:31 -0500

    fun

---
+++ modules/hellyeah.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'hellyeah',
+	aliases: ['hella', 'spoon'],
+	description: '"How to spoon: Dick hard on the butt. Titty in my hand. Kiss ya neck. Hell yeah."',
+	args: false,
+	cooldown: 3,
+	execute(message, args, bot) {
+		bot.sendMessage('https://imgur.com/63wf95x');
+	}
+};
