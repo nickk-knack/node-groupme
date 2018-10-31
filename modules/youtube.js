e6e17cd83cfcdad04ba73495fb964acc7988f2a9
nickk-knack <nick.chiconas@gmail.com>
2018-10-31 16:09:34 -0500

    add youtube search module

---
+++ modules/youtube.js
@@ -0,0 +1,30 @@
+// AIzaSyDVAvPqpW5iBxKPL56x0k5celxQC9gWH8g
+
+const search = require('youtube-search');
+
+const opts = {
+	maxResults: 10,
+	key: process.env.YOUTUBE_API_KEY,
+};
+
+module.exports = {
+	name: 'youtube',
+	aliases: ['yt'],
+	description: 'Search YouTube for videos.',
+	usage: '<search terms>',
+	args: true,
+	cooldown: 3,
+	execute(message, args, bot) {
+		const query = args.join(' ');
+
+		search(query, opts, (err, res) => {
+			if (err) {
+				bot.sendMessage('yo wtf is up');
+				return console.error(err);
+			}
+
+			console.log(res);
+			bot.sendMessage(res[Math.floor(Math.random() * res.length)].link);
+		});
+	},
+};
