fe9c361851c0bca7ab7b66c54536a197abc1bde8
Nick Chiconas <nick.chiconas@gmail.com>
2018-11-09 21:21:58 -0600

    small rewrite to giphy, increase search limit

--- modules/giphy.js
+++ modules/giphy.js
@@ -1,3 +1,5 @@
+const limit = 10;
+
 module.exports = {
 	name: 'giphy',
 	aliases: ['gif'],
@@ -7,11 +9,11 @@ module.exports = {
 	cooldown: 10,
 	execute(message, args, bot) {
 		const query = args.join(' ');
-		const url = `http://api.giphy.com/v1/gifs/search?limit=5&q=${encodeURIComponent(query)}&api_key=dc6zaTOxFJmzC`;
+		const url = `http://api.giphy.com/v1/gifs/search?limit=${limit}&q=${encodeURIComponent(query)}&api_key=dc6zaTOxFJmzC`;
 
 		bot.request.get(url, (err, resp, body) => {
 			const results = JSON.parse(body)['data'];
-			const numResults = (results.length < 5) ? results.length : 5;
+			const numResults = (results.length < limit) ? results.length : limit;
 			if (err || numResults == 0) {
 				bot.sendMessage(`Nothing found for "${query}"`);
 			} else {
