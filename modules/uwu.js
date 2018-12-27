bc94f0d7311d63324334cece1e743b4dab20135d
nickk-knack <nick.chiconas@gmail.com>
2018-12-26 20:27:57 -0600

    trim whitespace on query

--- modules/uwu.js
+++ modules/uwu.js
@@ -8,7 +8,7 @@ module.exports = {
 	args: true,
 	cooldown: 3,
 	execute(message, args, bot) {
-		const query = args.join('+');
+		const query = args.join('+').trim();
 		if (query == '') {
 			bot.sendMessage('UwU');
 			return;
