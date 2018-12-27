b912cdfcf083ea0ba767e321be49779b7616b1ec
nickk-knack <nick.chiconas@gmail.com>
2018-12-26 20:30:11 -0600

    trim whitespace on query, allow "video" in place of "vid"

--- modules/pornhub.js
+++ modules/pornhub.js
@@ -10,12 +10,12 @@ module.exports = {
 	execute(message, args, bot) {
 		const contentType = args.shift().toLowerCase();
 
-		if (contentType != 'gif' && contentType != 'vid') {
+		if (contentType != 'gif' && contentType != 'vid' && contentType != 'video') {
 			bot.sendMessage(`@${message.name} you need to specify if you want a gif or a vid.`);
 			return;
 		}
 		
-		const query = args.join(' ');
+		const query = args.join(' ').trim();
 		const search = new Pornsearch(query);
 		
 		if (contentType == 'gif') {
@@ -27,7 +27,7 @@ module.exports = {
 					console.error(error);
 					bot.sendMessage(`Could not find any gifs for ${query}`);
 				});
-		} else if (contentType == 'vid') {
+		} else if (contentType == 'vid' || contentType == 'video') {
 			search.videos()
 				.then(vids => {
 					bot.sendMessage(vids[Math.floor(Math.random() * vids.length)].url);
