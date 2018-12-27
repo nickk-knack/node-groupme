28ef1fe47fbd87862185a126ef6eb83606beafc1
nickk-knack <nick.chiconas@gmail.com>
2018-12-26 22:16:19 -0600

    different strengths of b replacement

--- modules/b.js
+++ modules/b.js
@@ -6,8 +6,24 @@ module.exports = {
 	cooldown: 3,
 	execute(message, args, bot) {
 		const b = '🅱️';
-		const bMessage = args.join(' ');
-		const regex = /[Bb]+/g;
+		const bMessage = args.join(' ').trim();
+
+		// normal strength b
+		let regex = /[b]+/gi;
+		const strength = Math.random();
+
+		if (strength > 0.8) {
+			// stronger b
+			regex = /[bp]+/gi;
+		} else if (strength < 0.1) {
+			// stronger b, replace up to 10 random characters
+			regex = /[bp]+/gi;
+
+			let randomReplacements = Math.floor(Math.random() * 10) + 1;
+			for (let i = 0; i < randomReplacements; i++) {
+				bMessage[Math.floor(Math.random() * bMessage.length())] = b;
+			}
+		}
 
 		bot.sendMessage(bMessage.replace(regex, b));
 	},
