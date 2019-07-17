0798a5d3feb264ee9725baa5e711ff8b5ccef53d
Nick Chiconas <NIck.Chiconas@gmail.com>
2019-07-17 09:19:45 -0500

    i think im done now

--- modules/uwuize.js
+++ modules/uwuize.js
@@ -10,13 +10,12 @@ module.exports = {
 
 // maybe just convert whole thing to lower
 
-                let uwu = original.replace('R', 'w');
-                uwu = uwu.replace(/rR/g, 'w');
-                uwu = uwu.replace(/lL/g, 'w');
+                let uwu = original.replace(/r/gi, 'w');
+                uwu = uwu.replace(/l/gi, 'w');
                 // add a case for first instance of a word that starts with Th (ignore case)
-                uwu = uwu.replace(/you/g, 'yuw'); // this might be extra
-                uwu = uwu.replace(/\sth/g, ' d');
-                uwu = uwu.replace(/th/g, 'f');
+                uwu = uwu.replace(/you/gi, 'yuw'); // this might be extra
+                uwu = uwu.replace(/\sth/gi, ' d');
+                uwu = uwu.replace(/th/gi, 'f');
 
                 bot.sendMessage(`${uwu} uwu`);
         }
