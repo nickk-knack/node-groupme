64ef523c96e0c4c8cf747b447d6c7f01ecb9d15d
nickk-knack <nick.chiconas@gmail.com>
2019-09-21 21:15:22 -0500

    add rate command

---
+++ modules/rate.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'rate',
+	aliases: ['r'],
+	description: 'Rates something out of 10.',
+	usage: '<thing>',
+	args: true,
+	cooldown: 3,
+	execute(message, args, bot) {
+		const overrate = Math.random() > 0.85;
+		const rate = overrate ? 11 : Math.floor((Math.random() * 10) + 1);
+
+		bot.sendMessage(`${args.join(' ')}?\nI'd rate that a ${rate}/10`);
+	},
+};
