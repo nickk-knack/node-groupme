7d4047b3e7b8cebc3bfea3f4e345bd8e5caa5246
nickk-knack <nick.chiconas@gmail.com>
2019-12-02 14:06:57 -0600

    rewrote to send image results through GMIS

--- modules/images.js
+++ modules/images.js
@@ -1,6 +1,9 @@
 const GoogleImages = require('google-images');
+const fetch = require('node-fetch');
 const GoogleAPIKey = process.env.GOOGLE_API_KEY;
 const GoogleCSEID = process.env.GOOGLE_CSE_ID;
+const GroupMeImageServiceAccessToken = process.env.GM_IMAGE_SERVICE_TOKEN;
+const gmisUrl = 'https://image.groupme.com/pictures';
 
 module.exports = {
 	name: 'image',
@@ -13,20 +16,45 @@ module.exports = {
 		const query = args.join(' ').trim();
 		const client = new GoogleImages(GoogleCSEID, GoogleAPIKey);
 
-		let result;
-		client.search(query).then(images => {
-			const length = images.length;
-			const randIndex = Math.floor(Math.random() * length);
-			result = images[randIndex].url;
+		client.search(query)
+			.then(images => {
+				const length = images.length;
+				const randIndex = Math.floor(Math.random() * length);
+				const url = images[randIndex].url;
+				const type = images[randIndex].type;
 
-			bot.sendMessage(result);
-		}).catch(e => {
-			console.error(e);
-			if (e.statusCode == 403) {
-				bot.sendMessage('I literally can\'t search anymore');
-			} else {
-				bot.sendMessage('oopsie woopsie, something is fucksie wucksies!! uwu');
-			}
-		});
+				fetch(url)
+					.then(res => res.buffer())
+					.then(buffer => {
+						const settings = {
+							method: 'POST',
+							body: buffer,
+							headers: {
+								'X-Access-Token': GroupMeImageServiceAccessToken,
+								'Content-Type': type
+							}
+						};
+						
+						fetch(gmisUrl, settings)
+							.then(resp => resp.json())
+							.then(json => bot.sendMessage('', json.payload.picture_url))
+							.catch(err => {
+								console.error('Failed to upload to GMIS, falling back to google link.', err);
+								return bot.sendMessage(url);
+							});
+					})
+					.catch(err => {
+						console.error('Failed to upload to GMIS, falling back to google link.', err);
+						return bot.sendMessage(url);
+					});
+			})
+			.catch(e => {
+				console.error(e);
+
+				// ternary abuse?
+				return (e.statusCode == 403) ? 
+					bot.sendMessage('I literally can\'t search anymore') : 
+					bot.sendMessage('oopsie woopsie, something is fucksie wucksies!! uwu');
+			});
 	},
 };
