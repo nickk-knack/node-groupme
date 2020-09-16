d41be1ba398d64971462c95f6f0da51f77bab79a
Nick Chiconas <nick.chiconas@gmail.com>
2020-09-15 23:04:08 -0500

    okay im done

--- modules/help.js
+++ modules/help.js
@@ -1,11 +1,25 @@
 const prefix = process.env.PREFIX || '.';
 
-// Add the map and find functions to the Map prototype
-Map.prototype.map = (fn, thisArg) => {
+// Find function for maps
+const find = (map, func) => {
+	if (typeof func === 'function') {
+		for (const [key, val] of map) {
+			if (func(val, key, map)) return val;
+		}
+
+		return null;
+	}
+	else {
+		throw new Error('First argument must be a function.');
+	}
+};
+
+// Map function for arrays
+const map = (m, fn, thisArg) => {
 	if (thisArg) fn = fn.bind(thisArg);
-	const arr = new Array(this.size);
+	const arr = new Array(m.size);
 	let i = 0;
-	for (const [key, val] of this) arr[i++] = fn(val, key, this);
+	for (const [key, val] of m) arr[i++] = fn(val, key, m);
 	return arr;
 };
 
@@ -20,10 +34,10 @@ module.exports = {
 		const { commands } = bot;
 
 		if (!args.length) {
-			return bot.sendMessage(`Commands: ${commands.map(cmd => cmd.name).join(', ')}\n You can send ${prefix}help [command name] for info on a specific command.`);
+			return bot.sendMessage(`Commands: ${map(commands, (cmd) => cmd.name).join(', ')}\n You can send ${prefix}help [command name] for info on a specific command.`);
 		}
 
-		const command = bot.commands.get(args[0]);
+		const command = bot.commands.get(args[0]) || find(commands, (cmd) => cmd.aliases && cmd.aliases.includes(args[0]));
 
 		if (!command) {
 			return bot.sendMessage(`@${message.name} "${args[0]}" is not a valid command or alias!`);
